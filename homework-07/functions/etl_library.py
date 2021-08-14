5fdcdce9ac4cb31b8d0ed44892035953c9e06f24
goncharevich_s <goncharevich_s@lwo.by>
2021-08-14 23:15:15 +0300

    copying to silver implementation

--- homework-07/functions/etl_library.py
+++ homework-07/functions/etl_library.py
@@ -1,6 +1,8 @@
 import logging
+import os
 from airflow.hooks.postgres_hook import PostgresHook
 from hdfs import InsecureClient
+from pyspark.sql import SparkSession
 
 
 def db_export(**kwargs):
@@ -8,13 +10,34 @@ def db_export(**kwargs):
     logging.info(f'Starting export of table {table_name}')
     config = kwargs['params']
     current_date = kwargs['ds']
-    hook = PostgresHook(postgres_conn_id=config['connId'])
+    hook = PostgresHook(postgres_conn_id=config['pgConnId'])
     client = InsecureClient(config['hdfs_url'], user=config['hdfs_user'])
     with hook.get_cursor() as cursor:
-        output_dir = config['outputDir'] + f'/{table_name}/{current_date}'
+        output_dir = config['bronzeOutputDir'] + f'/{table_name}/{current_date}'
         client.makedirs(output_dir)
         with client.write(hdfs_path=f'{output_dir}/{table_name}.csv',
                           overwrite=True
                           ) as output_file:
             cursor.copy_expert(f'COPY {table_name} TO STDOUT WITH HEADER CSV', output_file)
     logging.info(f'Export of table {table_name} successfully completed')
+
+
+def copy_to_silver(**kwargs):
+    table_name = kwargs['table_name']
+    logging.info(f'Start copying {table_name} to silver')
+    config = kwargs['params']
+    current_date = kwargs['ds']
+    spark = SparkSession \
+        .builder.master('local') \
+        .appName('homework-07') \
+        .getOrCreate()
+    df = spark.read.load(os.path.join(config['bronzeOutputDir'], table_name, current_date, table_name + '.csv')
+                         , header="true"
+                         , inferSchema="true"
+                         , format="csv")
+    # removing duplicate rows
+    df = df.drop_duplicates()
+
+    # writing result to silver
+    df.write.parquet(os.path.join(config['silverOutputDir'], table_name), mode='overwrite')
+    logging.info(f'Copying of table {table_name} to silver successfully completed')
--- homework-07/homework07_dag.py
+++ homework-07/homework07_dag.py
@@ -3,13 +3,15 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.operators.dummy_operator import DummyOperator
 from datetime import datetime
 from functions.etl_library import db_export
+from functions.etl_library import copy_to_silver
 
 hdfs_url = 'http://127.0.0.1:50070/'
 hdfs_user = 'user'
 
 dshop_config = {
-    'outputDir': '/bronze/dshop',
-    'connId': 'dshop',
+    'bronzeOutputDir': '/bronze/dshop',
+    'silverOutputDir': '/silver',
+    'pgConnId': 'dshop',
     'hdfs_url': hdfs_url,
     'hdfs_user': hdfs_user
 }
@@ -18,7 +20,7 @@ table_list = ('aisles', 'clients', 'departments', 'orders', 'products')
 
 db_dag = DAG(
     'homework_07_dshop',
-    start_date=datetime(2021, 8, 12),
+    start_date=datetime(2021, 8, 13),
     schedule_interval='@daily'
 )
 
@@ -34,16 +36,34 @@ def table_export_group(table_name):
     )
 
 
-dummy_start = DummyOperator(
+def copy_to_silver_group(table_name):
+    return PythonOperator(
+        dag=db_dag,
+        task_id=f'copy_to_silver_{table_name}',
+        provide_context=True,
+        python_callable=copy_to_silver,
+        params=dshop_config,
+        op_kwargs={'table_name': table_name}
+    )
+
+
+dummy_db_export_start = DummyOperator(
     task_id='db_export_start',
     dag=db_dag
 )
 
 
-dummy_end = DummyOperator(
+dummy_db_export_end = DummyOperator(
     task_id='db_export_end',
     dag=db_dag
 )
 
+
+dummy_silver_copying_end = DummyOperator(
+    task_id='dummy_silver_copying_end',
+    dag=db_dag
+)
+
+
 for table in table_list:
-    dummy_start >> table_export_group(table) >> dummy_end
+    dummy_db_export_start >> table_export_group(table) >> dummy_db_export_end >> copy_to_silver_group(table) >> dummy_silver_copying_end
